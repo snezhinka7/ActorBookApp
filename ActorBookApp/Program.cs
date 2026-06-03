ccf83264786126167dd6dcaf8c38b3420b76c3fd
Sofia Dominguez <135928742+snezhinka7@users.noreply.github.com>
2026-06-03 19:01:59 -0300

    Agregar archivos de proyecto.

---
+++ ActorBookApp/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine("Actores");
+
+var pelicula1 = new PeliculaEstelar();
+pelicula1.Nombre = "The Dark Night";
+pelicula1.Descripcion = "Batman se enfrenta a su mayor desafío";
+pelicula1.Nivel = NivelAlcance.Internacional;
+
+var person1 = new Actor();
+var person2 = new Actor();
+
+person1.Id = 1;
+person1.Nombre = "Christian Bale";
+person1.Oscar = true;
+List<PeliculaEstelar> peliculas = new List<PeliculaEstelar>();
+peliculas.Add(pelicula1);
+person1.Peliculas = peliculas;
+
+
+
+class Actor
+{
+    public int Id;
+    public string Nombre;
+    public bool Oscar;
+    public List<PeliculaEstelar> Peliculas;
+}
+
+class PeliculaEstelar
+{
+    public string Nombre;
+    public string Descripcion;
+    public NivelAlcance Nivel;
+}
+
+enum NivelAlcance
+{
+    Local,
+    Nacional,
+    Internacional
+}
